84312c1d9d9b73839c167722fc83248fb5c44d95
MathiasAKAchampen <201509795@post.au.dk>
2019-02-19 13:29:48 +0100

    klasse med substitute

---
+++ ECS.Unit.Tests/ECS.Test.Unit.Substitute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.Legacy.refacored;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ECS.Unit.Tests
+{
+    [TestFixture]
+    class ECS
+    {
+        private ECSRefactored UUT_;
+        private IHeater heater_;
+        private ITempSensor tempSensor_;
+
+        [SetUp]
+        public void setUp()
+        {
+            heater_ = Substitute.For<IHeater>();
+            tempSensor_ = Substitute.For<ITempSensor>();
+
+            UUT_ = new ECSRefactored(25,heater_,tempSensor_);
+        }
+    }
+}
